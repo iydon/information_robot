cb9454b745f2138885156747eaca53e234e7e0ef
Iydon <11711217@mail.sustc.edu.cn>
2018-09-30 10:36:54 +0800

    '->"

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import bus
 
 def extraFunction(contact,content,member, bot):
     # 附加功能
-    if 'bus' in content:
-        op = re.findall('\d+', content)
+    if "bus" in content:
+        op = re.findall("\d+", content)
         return 1,bus.bustime(int(op[0])-1 if op else None)
     return 0, ""
 
@@ -18,17 +18,17 @@ def extraFunction(contact,content,member, bot):
 @qqbotslot
 
 def onQQMessage(bot, contact, member, content):
-    #if getattr(member, 'uin', None) != bot.conf.qq:
+    #if getattr(member, "uin", None) != bot.conf.qq:
     if not bot.isMe(contact, member):
-        if content[0] in [';','；'] and content[-1] in [';','；']:
-            with open('data.tsv',mode='a+', encoding='utf-8') as f:
+        if content[0] in [";","；"] and content[-1] in [";","；"]:
+            with open("data.tsv",mode="a+", encoding="utf-8") as f:
                 string = content[1:-1]
-                f.write(string.replace('；', '\t').replace(';', '\t')+'\n')
+                f.write(string.replace("；", "\t").replace(";", "\t")+"\n")
                 return
-        with open('data.tsv',encoding='utf-8') as f:
-            keywords = [re.split('\t+', j) for j in re.split('[\r\n]+', f.read().strip())]
+        with open("data.tsv",encoding="utf-8") as f:
+            keywords = [re.split("\t+", j) for j in re.split("[\r\n]+", f.read().strip())]
         for keyword in keywords:
-            if '#' == keyword[0]:
+            if "#" == keyword[0]:
                 continue
             if len(keyword) > 1:
                 or_flag = True
@@ -45,7 +45,7 @@ def onQQMessage(bot, contact, member, content):
                         or_flag = False
                         break
                 if or_flag:
-                    if keyword[-1][0]==':':
+                    if keyword[-1][0]==":":
                         exec("from pluger import {0} as tmp_module".format(keyword[-1][1:]))
                         string = eval("tmp_module.onMessage(bot,contact,member,content)")
                         bot.SendTo(contact, string)
@@ -57,11 +57,11 @@ def onQQMessage(bot, contact, member, content):
             bot.SendTo(contact, con)
             return
         # shutdown
-        if 'sudo rm -rf /' == content or ':{:|:&};:' == content or '%0|%0' == content:
-            bot.SendTo(contact, '我要休息一下~')
+        if "sudo rm -rf /" == content or ":{:|:&};:" == content or "%0|%0" == content:
+            bot.SendTo(contact, "我要休息一下~")
             bot.Stop()
             return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     RunBot()
